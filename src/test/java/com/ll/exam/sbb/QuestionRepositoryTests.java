d53d7d4dd30f2291e79e47334a4addadff08d5ed
JeongHyeSoo <nailonly@naver.com>
2022-09-12 19:48:52 +0900

    저장 삭제 테스트 앞서 만든 BeforeEach로 삭제 반복에도 독립적 환경 보장

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -1,10 +1,13 @@
 package com.ll.exam.sbb;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
@@ -40,6 +43,36 @@ public class QuestionRepositoryTests {
         questionRepository.save(q2);
 
         lastSampleDataId = q2.getId();
+    }
+
+    @Test
+        //본 어노테이션을 붙이면 Test 메서드로 인식하고 테스트 한다.
+        //JUnit5 기준으로 접근제한자가 Default 여도 된다. (JUnit4 까지는 public이어야 했었다.)
+    void 저장(){
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);
+
+        assertThat(q1.getId()).isEqualTo(lastSampleDataId + 1);
+        assertThat(q2.getId()).isEqualTo(lastSampleDataId + 2);
+    }
+
+    @Test
+    void 삭제() {
+        assertThat(questionRepository.count()).isEqualTo(lastSampleDataId);
+
+        Question q = this.questionRepository.findById(1).get();
+        questionRepository.delete(q);
+
+        assertThat(questionRepository.count()).isEqualTo(lastSampleDataId - 1);
     }
 }
 
