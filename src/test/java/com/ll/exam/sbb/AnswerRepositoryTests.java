9e0d812cb92c56c6da13ae343c8b9d463208320b
JeongHyeSoo <nailonly@naver.com>
2022-09-13 02:44:27 +0900

    인터페이스 다중 상속을 통해 외래키 끊어내는 코드의 중복을 해결

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -27,9 +27,11 @@ public class AnswerRepositoryTests {
     private void clearData() {
         QuestionRepositoryTests.clearData(questionRepository);
         //questionRepository truncate는 이제 questionRepositoryTests의 clearData에서 실행한다.
-        questionRepository.disableForeignKeyChecks();
+        answerRepository.disableForeignKeyChecks();
         answerRepository.truncate();
-        questionRepository.enableForeignKeyChecks();
+        answerRepository.enableForeignKeyChecks();
+        //RepositoryUtil을 인터페이스 다중 상속을 통해 QuestionRepository와 EnswerRepository 모두
+        // 각자의 repository를 참조하여 외래키 관계를 끊을 수 있도록 변경
     }
 
     private void createSampleData() {
