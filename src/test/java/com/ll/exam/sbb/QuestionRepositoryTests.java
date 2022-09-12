7b73e59002f16f2f1bc50a9ffe78272aa454f109
JeongHyeSoo <nailonly@naver.com>
2022-09-12 20:27:52 +0900

    조회 테스트(findAll 등)

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -86,6 +86,35 @@ public class QuestionRepositoryTests {
 
         assertThat(q.getSubject()).isEqualTo("수정된 제목");
     }
+    @Test
+    void findAll() {
+        List<Question> all = questionRepository.findAll();
+        assertThat(all.size()).isEqualTo(lastSampleDataId);
+
+        Question q = all.get(0);
+        assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
+    }
+
+    @Test
+    void findBySubject() {
+        Question q = questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertThat(q.getId()).isEqualTo(1);
+    }
+
+    @Test
+    void findBySubjectAndContent() {
+        Question q = questionRepository.findBySubjectAndContent(
+                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertThat(q.getId()).isEqualTo(1);
+    }
+
+    @Test
+    void findBySubjectLike() {
+        List<Question> qList = questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+
+        assertThat(q.getSubject()).isEqualTo("sbb가 무엇인가요?");
+    }
 }
 
 /*
