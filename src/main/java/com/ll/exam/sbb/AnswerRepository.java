08ad2c457b944154fa61c45ba96b7bd41143c6f3
JeongHyeSoo <nailonly@naver.com>
2022-09-13 03:31:45 +0900

    TRUNCATE 대신 AUTO_INCREMENT를 강제로 1로 지정

--- src/main/java/com/ll/exam/sbb/AnswerRepository.java
+++ src/main/java/com/ll/exam/sbb/AnswerRepository.java
@@ -10,5 +10,9 @@ public interface AnswerRepository extends JpaRepository<Answer, Integer>, Reposi
     @Transactional
     @Modifying
     @Query(value = "ALTER TABLE answer AUTO_INCREMENT = 1", nativeQuery = true)
+    /*
+    truncate는 jpa와 궁합이 안좋다. 데이터 삭제(delete) 할 때
+    auto_increment(ID)를 강제로 1로 지정함으로 truncate와 같은 효과를 낸다.
+    */
     void truncate();// 이거 지우면 안됨, truncateTable 하면 자동으로 이게 실행됨
 }
