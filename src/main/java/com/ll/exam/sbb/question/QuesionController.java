e90d1c1a54b4161fa384de5a9a23e54a87829094
JeongHyeSoo <nailonly@naver.com>
2022-09-13 09:35:50 +0900

    QuestionController 추가

---
+++ src/main/java/com/ll/exam/sbb/question/QuesionController.java
@@ -0,0 +1,11 @@
+package com.ll.exam.sbb.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class QuesionController {
+
+
+}
