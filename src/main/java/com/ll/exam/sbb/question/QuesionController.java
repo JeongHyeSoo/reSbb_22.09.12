eab5e0e4aba2033a6a9ff36d3484a20074146141
JeongHyeSoo <nailonly@naver.com>
2022-09-13 09:40:12 +0900

    question_list.html 뷰 파일 추가

--- src/main/java/com/ll/exam/sbb/question/QuesionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuesionController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuesionController {
-
-
+    @RequestMapping("/question/list")
+    /* 이 자리에 @ResponseBody가 없으면
+    resources/question_list/question_list.html 파일을 뷰로 삼는다.
+    */
+    public String list(){
+        return "question_list";
+    }
 }
