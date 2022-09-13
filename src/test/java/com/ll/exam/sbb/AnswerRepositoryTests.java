975e7d682c5f862b749bed49e4c827c4c1ea33af
JeongHyeSoo <nailonly@naver.com>
2022-09-13 09:30:15 +0900

    앞서 한 객체지향적 방법으로 새 데이터 저장

--- src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/AnswerRepositoryTests.java
@@ -67,11 +67,17 @@ public class AnswerRepositoryTests {
     void 저장() {
         Question q = questionRepository.findById(2).get();
 
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);
-        a.setCreateDate(LocalDateTime.now());
-        answerRepository.save(a);
+        Answer a1 = new Answer();
+        a1.setContent("네 자동으로 생성됩니다.");
+        a1.setCreateDate(LocalDateTime.now());
+
+        q.addAnswer(a1);
+
+        Answer a2 = new Answer();
+        a2.setContent("네네 맞아요!!~:)");
+        a2.setCreateDate(LocalDateTime.now());
+
+        q.addAnswer(a2);
     }
 
     @Test
