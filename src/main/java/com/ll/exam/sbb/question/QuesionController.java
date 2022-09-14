590a2d75b902a46db4cd296f48163f1089aeb832
JeongHyeSoo <nailonly@naver.com>
2022-09-14 13:50:04 +0900

    폼 밸리데이션 적용하여 question 기존 체크 로직 개선

--- src/main/java/com/ll/exam/sbb/question/QuesionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuesionController.java
@@ -3,11 +3,13 @@ package com.ll.exam.sbb.question;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/question")
@@ -56,31 +58,22 @@ public class QuesionController {
 
 
     @GetMapping("/create")
-    public String questionCreate() {
+    public String questionCreate(QuestionForm questionForm) {
         return "question_form";
     }
 
     @PostMapping("/create")
     //controller가 model이라는 객체를 파라미터로 받게 하는 메서드
-    public String questionCreate(Model model, QuestionFrom questionFrom) {
-        boolean hasError = false;
+    public String questionCreate(Model model, @Valid QuestionForm questionForm, BindingResult bindingResult) {
 
-        if (questionFrom.getSubject() == null || questionFrom.getSubject().trim().length() == 0) {
-            model.addAttribute("subjectErrorMsg", "제목 좀...");
-            hasError = true;
-        }
 
-        if (questionFrom.getContent() == null || questionFrom.getContent().trim().length() == 0) {
-            model.addAttribute("contentErrorMsg", "내용 좀...");
-            hasError = true;
-        }
 
-        if (hasError) {
-            model.addAttribute("questionFrom", questionFrom);
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("questionFrom", questionForm);
             return "question_form";
         }
 
-        questionService.create(questionFrom.getSubject(), questionFrom.getContent());
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
 }
