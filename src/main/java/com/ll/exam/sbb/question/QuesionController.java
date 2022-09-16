f093ddea7f56766bc19d9ca438c79296f473ad01
JeongHyeSoo <nailonly@naver.com>
2022-09-16 14:14:10 +0900

    questionService.getList 메서드가 모든 질문이 아닌, 현재 페이지에 맞는 10개의 질문을 Page객체에 담아서 반환

--- src/main/java/com/ll/exam/sbb/question/QuesionController.java
+++ src/main/java/com/ll/exam/sbb/question/QuesionController.java
@@ -2,13 +2,11 @@ package com.ll.exam.sbb.question;
 
 import com.ll.exam.sbb.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -32,18 +30,18 @@ import java.util.List;
 public class QuesionController {
     // @Autowired // 필드 주입
     private final QuestionService questionService;
-    @RequestMapping("/list")
+    @GetMapping("/list")
     /* 이 자리에 @ResponseBody가 없으면
     resources/question_list/question_list.html 파일을 뷰로 삼는다.
     */
-    public String list(Model model) {
-        List<Question> questionList = questionService.getList();
-        //모든 question들을 조회해 리스트로 만든다.
+    public String list(Model model, @RequestParam(defaultValue = "0") int page) {
+        Page<Question> paging = questionService.getList(page);
+        //question들을 조회해 리스트로 만든다.
         /*
         미래에 실행될 question_list.html 에서
         questionList 라는 이름으로 questionList 변수를 사용할 수 있다.
         */
-        model.addAttribute("questionList", questionList);
+        model.addAttribute("paging", paging);
         //questionList를 question_list에 전달해주는 스프링부트의 전용 변수 - Model
 
             return "question_list";
