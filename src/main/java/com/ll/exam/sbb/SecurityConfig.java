6e410d963ccd3997b5928163c1054a4b06645d8c
JeongHyeSoo <nailonly@naver.com>
2022-09-19 01:00:43 +0900

    로그인 폼 구현

--- src/main/java/com/ll/exam/sbb/SecurityConfig.java
+++ src/main/java/com/ll/exam/sbb/SecurityConfig.java
@@ -21,7 +21,11 @@ public class SecurityConfig {
                 .csrf().ignoringAntMatchers("/h2-console/**")
                 .and()
                 .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(
-                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+                .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/");
         return http.build();
     }
     // 스프링 시스템에 객체를 등록한다.
