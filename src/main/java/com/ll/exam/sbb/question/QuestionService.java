79f791b29a4eab951d7a03713a3063462c1350c8
JeongHyeSoo <nailonly@naver.com>
2022-09-21 01:51:00 +0900

    삭제 버튼

--- src/main/java/com/ll/exam/sbb/question/QuestionService.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionService.java
@@ -45,16 +45,4 @@ public class QuestionService {
         q.setCreateDate(LocalDateTime.now());
         questionRepository.save(q);
     }
-
-
-    public void modify(Question question, String subject, String content) {
-        question.setSubject(subject);
-        question.setContent(content);
-        question.setModifyDate(LocalDateTime.now());
-        questionRepository.save(question);
-    }
-
-    public void delete(Question question) {
-        this.questionRepository.delete(question);
-    }
 }
